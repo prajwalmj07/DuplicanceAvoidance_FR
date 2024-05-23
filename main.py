0568af008c50970332ceef425ae4fbf192a997ce
Prajwal <prajwalmjain5914@gmail.com>
2024-05-23 22:55:54 +0530

    util

---
+++ main.py
@@ -0,0 +1,50 @@
+import os.path
+import datetime
+import pickle
+
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+import face_recognition
+
+import util
+
+
+
+class App:
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.main_window.geometry("1200x520+350+100")
+
+        self.login_button_main_window = util.get_button(self.main_window, 'login', 'green', self.login)
+        self.login_button_main_window.place(x=750, y=200)
+
+        self.logout_button_main_window = util.get_button(self.main_window, 'logout', 'red', self.logout)
+        self.logout_button_main_window.place(x=750, y=300)
+
+        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',
+                                                                    self.register_new_user, fg='black')
+        self.register_new_user_button_main_window.place(x=750, y=400)
+
+        self.webcam_label = util.get_img_label(self.main_window)
+        self.webcam_label.place(x=10, y=0, width=700, height=500)
+
+        self.add_webcam(self.webcam_label)
+
+        self.db_dir = './db'
+        if not os.path.exists(self.db_dir):
+            os.mkdir(self.db_dir)
+
+        self.log_path = './log.txt'
+
+    def add_webcam(self, label):
+        if 'cap' not in self.__dict__:
+            self.cap = cv2.VideoCapture(2)
+
+        self._label = label
+        self.process_webcam()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
