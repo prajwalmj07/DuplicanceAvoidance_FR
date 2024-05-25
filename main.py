e8c47e52d8430d5ae26875f2ba17f89c72f79db1
Prajwal <prajwalmjain5914@gmail.com>
2024-05-26 00:10:34 +0530

    add buttons/cam

--- main.py
+++ main.py
@@ -39,10 +39,35 @@ class App:
 
     def add_webcam(self, label):
         if 'cap' not in self.__dict__:
-            self.cap = cv2.VideoCapture(2)
+            self.cap = cv2.VideoCapture(0)
 
         self._label = label
         self.process_webcam()
+
+    def process_webcam(self):
+        ret, frame = self.cap.read()
+
+        self.most_recent_capture_arr = frame
+        img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
+        self.most_recent_capture_pil = Image.fromarray(img_)
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+        self._label.imgtk = imgtk
+        self._label.configure(image=imgtk)
+
+        self._label.after(20, self.process_webcam)
+
+    def login(self):
+
+       pass
+
+    def register_new_user(self):
+
+        pass
+    def logout(self):
+
+        pass
+    def start(self):
+       self.main_window.mainloop()
 
 
 if __name__ == "__main__":
